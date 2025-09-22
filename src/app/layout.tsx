a3481f78024690dbb1c19a23db6fd5537650a556
Gurwinder2005 <gs2968884@gmail.com>
2025-09-22 16:02:48 +0530

    Vercel changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import { Poppins } from 'next/font/google';
